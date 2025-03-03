1bae11f671e5b4896bade203da1bac99b255f5d5
Pagnol_wesley <emperorb007@gmail.com>
2025-03-03 06:22:55 +0100

    Initialisation Clerk terminer

--- app/components/AuthWrapper.tsx
+++ app/components/AuthWrapper.tsx
@@ -2,11 +2,20 @@ type WrapperProps = {
   children: React.ReactNode
 }
 
+import { FolderGit2 } from 'lucide-react'
 import React from 'react'
 
 const AuthWrapper = ({children} : WrapperProps) => {
   return (
     <div className='h-screen flex justify-center items-center flex-col'>
+        <div className='flex items-center mb-6'>
+            <div className='bg-primary-content text-primary rounded-full p-2'>
+                <FolderGit2 size={24} />
+            </div>
+            <span className='ml-3 font-bold text-3xl'>
+                Strat<span className="text-primary">Edge</span>
+            </span>
+        </div>
         <div>
             {children}
         </div>
